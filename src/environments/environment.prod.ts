56ff5a1b97a4ca91b1d964fd91da95be0038457b
sk <nothing225200@gmail.com>
2025-02-23 14:21:32 +0530

    c

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_KEY:'AIzaSyDHfr_zckqS038lyc7F-7oHztO4ezi0bmw'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  API_KEY:'AIzaSyDHfr_zckqS038lyc7F-7oHztO4ezi0bmw'
-};
-
